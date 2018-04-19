845cdb3a9b65a80460a56ee3a2b891868cf9089d
Rico Callirgos <rico.callirgos@praxent.com>
2018-04-19 17:20:28 -0500

    Loads project assignments
    Signed-off-by: Rico Callirgos <rico.callirgos@praxent.com>

--- js/logHours.js
+++ js/logHours.js
@@ -4,53 +4,85 @@
 
 'use strict';
 
-let logHours = document.getElementById('logHours');
-let fromDateElement = document.getElementById('fromDate');
-let toDateElement = document.getElementById('toDate');
-
 $(function() {
-  let fromDate = getCurrentDate();
-  let toDate = getCurrentDate();
-  fromDateElement.value = fromDate;
-  toDateElement.value = toDate;
-});
+  let $btnLogHours = $('#logHours');
+  let $fromDateElement = $('#fromDate');
+  let $toDateElement = $('#toDate');
+  $fromDateElement.attr('value', getCurrentDate());
+  $toDateElement.attr('value', getCurrentDate());
+  $btnLogHours.attr('disabled', true);
 
-logHours.onclick = function(element) {
-  getCurrentHours(fromDateElement.value, toDateElement.value);
-};
+  let userId = 2814;
+  loadProjectAssignments(userId);
 
-function getCurrentDate() {
-  var fullDate = new Date();
-  var twoDigitMonth = fullDate.getMonth()+1+"";if(twoDigitMonth.length==1)  twoDigitMonth="0" +twoDigitMonth;
-  var twoDigitDate = fullDate.getDate()+"";if(twoDigitDate.length==1) twoDigitDate="0" +twoDigitDate;
-  var currentDate = fullDate.getFullYear() + "-" +  twoDigitMonth + "-" +  twoDigitDate;
+  $btnLogHours.onclick = function(element) {
+    getCurrentHours(fromDateElement.value, toDateElement.value);
+  };
 
-  return currentDate;
-}
-function getCurrentHours(fromDate, toDate, isProd = false) {
-  var additionalParameters = '&from=' + fromDate + '&to=' + toDate;
+  function getCurrentDate() {
+    var fullDate = new Date();
+    var twoDigitMonth = fullDate.getMonth()+1+"";if(twoDigitMonth.length==1)  twoDigitMonth="0" +twoDigitMonth;
+    var twoDigitDate = fullDate.getDate()+"";if(twoDigitDate.length==1) twoDigitDate="0" +twoDigitDate;
+    var currentDate = fullDate.getFullYear() + "-" +  twoDigitMonth + "-" +  twoDigitDate;
 
-  var prodUserId = '2814';
-  var prodEndpoint = '/api/v1/users/' + prodUserId + '/time_entries';
-  var prodAuthToken = 'bk93NGlXOVVIbWZoaDFYdHJKTE01VTVTcGVGNWlxRmRsUC8wcXpKNlBXcUd3OFh3b3BveWJqRlJyZzVOClhzRWE4Vm8xYTBSTkxjVHlQS1lEbXpCaFp4eUFMOGkvZHQ2ekFHcGRNUFBkRVZla2xOK1plemVjc04ySApFMVYzaXkzbgo=';
-  var prodUrl = 'https://api.10000ft.com' + prodEndpoint + '?auth=' + prodAuthToken + additionalParameters;
+    return currentDate;
+  }
+  function loadProjectAssignments(userId) {
+    let endpoint = getProjectAssignmentEndpointForUser(userId);
+    let apiUrl = getApiUrlForEndpoint(endpoint, 'staging');
 
-  var stagingUserId = '2814';
-  var stagingEndpoint = '/api/v1/users/' + stagingUserId + '/time_entries';
-  var stagingAuthToken = 'WEwrN0Z6ZHJuMTNYU2lBMnF5ajJ4aW1icTd6OS9aM2RQNGMvWDVrRU5pSVprTHNlYVZyVHRQT1NUb1M0CkVWYk9ocFIxQmg4UGRxWVNVZTFZbm0raUl5ekNnSlZ3eStlNEVtb09OSlNOQi9zdXNHMUxxK1FBYXVCdgpnZXJIZ0tTSAo=';
-  var stagingUrl = 'https://vnext-api.10000ft.com' + stagingEndpoint + '?auth=' + stagingAuthToken + additionalParameters;
+    $.ajax({
+      url: apiUrl,
+      type : "GET",
+      headers: { "Content-Type": "application/json" },
+      success: function(response) {
+        $btnLogHours.text('Log Hours');
+        $btnLogHours.attr('disabled', false);
+        console.log(response);
+        console.log('READY!');
+      },
+      error: function(error) {
+        $btnLogHours.text('Error...');
+        console.log(error);
+      }
+    });
+  }
 
-  var url = (isProd) ? prodUrl : stagingUrl;
+  function getCurrentHours(fromDate, toDate, isProd = false) {
+    var additionalParameters = '&from=' + fromDate + '&to=' + toDate;
 
-  $.ajax({
-    url: url,
-    type : "GET",
-    headers: { "Content-Type": "application/json" },
-    success: function(response) {
-      console.log(response);
-    },
-    error: function(error) {
-      console.log(error);
+    var url = (isProd) ? prodUrl : stagingUrl;
+
+    $.ajax({
+      url: url,
+      type : "GET",
+      headers: { "Content-Type": "application/json" },
+      success: function(response) {
+        console.log(response);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
+  }
+
+  function getTimeEntryEndpointForUser(userId) {
+    return '/api/v1/users/' + userId + '/time_entries';
+  }
+
+  function getProjectAssignmentEndpointForUser(userId) {
+    return '/api/v1/users/' + userId + '/assignments';
+  }
+
+  function getApiUrlForEndpoint(endpoint, env = 'staging') {
+    if (env == 'prod') {
+      var prodAuthToken = 'bk93NGlXOVVIbWZoaDFYdHJKTE01VTVTcGVGNWlxRmRsUC8wcXpKNlBXcUd3OFh3b3BveWJqRlJyZzVOClhzRWE4Vm8xYTBSTkxjVHlQS1lEbXpCaFp4eUFMOGkvZHQ2ekFHcGRNUFBkRVZla2xOK1plemVjc04ySApFMVYzaXkzbgo=';
+
+      return 'https://api.10000ft.com' + endpoint + '?auth=' + prodAuthToken;
     }
-  });
-}
+
+    var stagingAuthToken = 'WEwrN0Z6ZHJuMTNYU2lBMnF5ajJ4aW1icTd6OS9aM2RQNGMvWDVrRU5pSVprTHNlYVZyVHRQT1NUb1M0CkVWYk9ocFIxQmg4UGRxWVNVZTFZbm0raUl5ekNnSlZ3eStlNEVtb09OSlNOQi9zdXNHMUxxK1FBYXVCdgpnZXJIZ0tTSAo=';
+
+    return 'https://vnext-api.10000ft.com' + endpoint + '?auth=' + stagingAuthToken;
+  }
+});
